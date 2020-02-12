8114d2976e75c34c786a16cee86f75a25748c628
portfwd <60975463+port11@users.noreply.github.com>
2020-02-12 19:09:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 apt install figlet toilet php -y
-echo PORTFORWARD ACTIVATED!
+echo PfwD and net +20 % speed ACTIVATED!
+echo -en '\E[47;31m'"\033[1mInternet\033[0m"
+echo -en '\E[47;31m'"\033[1mSpeed 20%\033[0m"
 echo -en '\E[47;31m'"\033[1mActivated\033[0m"
 php portlocal.php
-
