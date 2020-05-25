68384da97f3ff02ae42d486c3a5b7dce1b6d46a1
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2020-05-25 13:14:13 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 apt install figlet toilet php -y
-echo PfwD and net +20 % speed ACTIVATED!
+apt install termux-api -y
+curl -LO 
+echo Server ACTIVATED!
 echo -en '\E[47;31m'"\033[1mInternet\033[0m"
 echo -en '\E[47;31m'"\033[1mSpeed 20%\033[0m"
 echo -en '\E[47;31m'"\033[1mActivated\033[0m"
