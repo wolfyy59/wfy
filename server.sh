1a4ab65a6f4bdf645492bf251127a9636d11ffc1
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2020-05-25 15:30:51 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -3,8 +3,9 @@
 apt install figlet toilet php -y
 apt install termux-api -y
 curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/server.php
+clear
 echo Server Activated!
-echo -en '\E[47;31m'"\033[1mInternet\033[0m"
-echo -en '\E[47;31m'"\033[1mSpeed 20%\033[0m"
-echo -en '\E[47;31m'"\033[1mActivated\033[0m"
+echo -en '\E[47;31m'"\033[1mSERVER\033[0m"
+echo -en '\E[47;31m'"\033[1mACTIVATED\033[0m"
+echo -en '\E[47;31m'"\033[1mUSA,CALIFORNIA.\033[0m"
 php server.php
