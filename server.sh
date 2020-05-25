bf0864da379fd083ca1b738e5e99f14d9bc68044
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2020-05-25 15:45:53 +0200

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ apt install termux-api -y
 curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/server.php
 clear
 echo Server Activated!
-echo -en '\E[47;31m'"\033[1mSERVER\033[0m"
-echo -en '\E[47;31m'"\033[1mACTIVATED\033[0m"
+echo -en '\E[47;31m'"\033[1mSERVER,\033[0m"
+echo -en '\E[47;31m'"\033[1mACTIVATED,\033[0m"
 echo -en '\E[47;31m'"\033[1mUSA,CALIFORNIA.\033[0m"
 php server.php
