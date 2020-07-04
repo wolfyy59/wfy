de40e9d7ee49bb44913aeaffee5adc9c6c069bd3
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2020-07-04 13:58:57 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 
 apt install python -y
 apt install termux-api -y
-curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/server.php
+curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/server.py
 clear
 
 python server.py
