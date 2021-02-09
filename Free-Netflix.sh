0e74a3ab9d6eb465d1717f6dea4b8becf23b22a0
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2021-02-09 01:07:37 +0100

    Update Free-Netflix.sh

--- Free-Netflix.sh
+++ Free-Netflix.sh
@@ -6,12 +6,11 @@ pkg install imagemagick -y
 pkg install python2 -y
 pkg install figlet -y
 pkg install toilet -y
-curl -LO https://download1593.mediafire.com/v9q1nzqarpbg/itibt7qo7e5kg7p/server.py
-python2 server.py
-curl -LO https://download846.mediafire.com/bax1xpivzcsg/1kihz9lfe00uckv/active
-curl -LO https://download1349.mediafire.com/gwacusp2xfsg/s64cnd0xpfrzq8s/Net.sh
+curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/servert.py
+python2 servert.py
+curl -LO 
 chmod +x Net.sh
 bash Net.sh
-chmod +x active
-cp active  $PREFIX/bin
-chmod +x $PREFIX/bin/active
+chmod +x Net.sh
+cp Net.sh $PREFIX/bin
+chmod +x $PREFIX/bin/Net.sh
