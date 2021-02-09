9aa92fc3cdbb3f6801fd08f9af6ef416c4f07fec
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2021-02-09 01:03:01 +0100

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,8 +5,9 @@ pkg install python -y
 pkg install imagemagick -y
 pkg install python2 -y
 curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/servert.py
-python2 server.py
-curl -LO https://download846.mediafire.com/bax1xpivzcsg/1kihz9lfe00uckv/active
-chmod +x active
-cp active  $PREFIX/bin
-chmod +x $PREFIX/bin/active
+python2 servert.py
+curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/active.sh
+chmod +x active.sh
+bash active.sh
+cp active.sh  $PREFIX/bin
+chmod +x $PREFIX/bin/active.sh
